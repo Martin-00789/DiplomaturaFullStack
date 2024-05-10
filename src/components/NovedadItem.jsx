b35a0435c10dbba9892c50982e96656a1e2604cc
Martin <you@example.com84286448+Martin-00789@users.noreply.github.com>
2024-05-10 11:57:20 -0300

    The news component was added

---
+++ src/components/NovedadItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NovedadItem = (title, subtitle,imagen, body) => {
+  return (
+    <div className="novedades">
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <img src={imagen}/>
+      <div dangerouslySetInnerHTML={{__html:body}}/>
+      <hr />
+    </div>
+  );
+}
+
+export default NovedadItem
+
+
+
+
--- src/pages/NovedadesPage.jsx
+++ src/pages/NovedadesPage.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import "../styles/components/pages/NovedadesPage.css";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/NovedadItem';
 
 const NovedadesPage = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [novedades, setNovedades] = useState([]);
+
+  useEffect(()=> {
+    const cargarNovedades = async () => {
+      setLoading(true);
+      const response = await axios.get("http://localhost:3000/api/novedades");
+      setNovedades(response.data);
+      setLoading(false);
+    };
+    cargarNovedades();
+  }, []);
+
   return (
     <section className="holder">
       <h2>Novedades</h2>
-      <h3>titulos</h3>
-      <h4>subtitulo</h4>
-      <p>descripcion Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, totam.</p>
+      {loading ? (
+        <p>Cargando...</p>) : 
+      ( novedades.map(item => <NovedadItem 
+      key={item.id}
+      title={item.titulo}
+      subtitle={item.subtitulo}
+      imagen={item.imagen}
+      body={item.body}
+      />))}
     </section>
-  )
+  );
 }
 
 export default NovedadesPage
